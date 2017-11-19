3f6fa543c52966346850e01d6a56e49f723bbdfd
Yang Fan <yangfan789@outlook.com>
2017-11-20 01:14:34 +0800

    Add Brainfuck Interpreter

---
+++ src/BrainfuckInterpreter/BrainfuckInterpreter/Interpreter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace BrainfuckInterpreter
+{
+    internal class Interpreter
+    {
+        private readonly char[] _memory;
+
+        public Interpreter(int memorySize = 1000)
+        {
+            _memory = new char[memorySize];
+        }
+
+        public void Execute(string code)
+        {
+            var currentMemoryPosition = 0;
+            var pos = 0;
+            while (pos < code.Length)
+            {
+                var ins = code[pos];
+                switch (ins)
+                {
+                    case '>':
+                        currentMemoryPosition++;
+                        break;
+                    case '<':
+                        currentMemoryPosition--;
+                        break;
+                    case '+':
+                        _memory[currentMemoryPosition]++;
+                        break;
+                    case '-':
+                        _memory[currentMemoryPosition]--;
+                        break;
+                    case '.':
+                        Console.Write(_memory[currentMemoryPosition]);
+                        break;
+                    case ',':
+                        _memory[currentMemoryPosition] = (char)Console.Read();
+                        break;
+                    case '[':
+                        if (_memory[currentMemoryPosition] == 0)
+                        {
+                            while (code[pos] != ']')
+                            {
+                                pos++;
+                            }
+                        }
+                        break;
+                    case ']':
+                        if (_memory[currentMemoryPosition] != 0)
+                        {
+                            while (code[pos] != '[')
+                            {
+                                pos--;
+                            }
+                        }
+                        break;
+                }
+                pos++;
+            }
+        }
+    }
+}
---
+++ src/BrainfuckInterpreter/BrainfuckInterpreter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BrainfuckInterpreter
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Brainfuck Intepreter. Written by fuis. 2016-12-2");
+            var interpreter = new Interpreter();
+            while (true)
+            {
+                Console.Write("$ ");
+                interpreter.Execute(Console.ReadLine());
+            }
+        }
+    }
+}
